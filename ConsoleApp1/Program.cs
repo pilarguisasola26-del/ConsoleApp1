9e17387b4f6fd3d92645a0692ee3c56fab883ce3
pili_ <pili_@GUISASOLA>
2026-08-14 12:14:40 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// ETAPA 1 
+string name = "KIOSCO EL RECREO";
+Console.WriteLine(name);
+
+Console.WriteLine("ingrese su nombre: ");
+string user = Console.ReadLine();
+Console.WriteLine ($"bienevenida {user}. la caja esta abierta");
+Console.ReadLine();
+
+//ETAPA 2 
+string Producto = ".";
+Console.WriteLine(Producto);
+Console.WriteLine("ingrese nombre del producto: ");
+string Product = Console.ReadLine();
